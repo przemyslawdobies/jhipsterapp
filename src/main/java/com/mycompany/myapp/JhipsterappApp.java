8430df57e8e6de82d1fb83f7b141c6fcdc71f695
przemyslawdobies <dobies.przemyslaw@gmail.com>
2020-02-08 13:14:39 +0100

    Zmiana powitania w logu

--- src/main/java/com/mycompany/myapp/JhipsterappApp.java
+++ src/main/java/com/mycompany/myapp/JhipsterappApp.java
@@ -81,7 +81,7 @@ public class JhipsterappApp {
             log.warn("The host name could not be determined, using `localhost` as fallback");
         }
         log.info("\n----------------------------------------------------------\n\t" +
-                "Application '{}' is running! Access URLs:\n\t" +
+                "Aplikacja '{}' dziala! Access URLs:\n\t" +
                 "Local: \t\t{}://localhost:{}{}\n\t" +
                 "External: \t{}://{}:{}{}\n\t" +
                 "Profile(s): \t{}\n----------------------------------------------------------",
